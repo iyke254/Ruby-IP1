88bc3d7e5cc39db8481f530de9437a0ae5458d9c
iyke <nderiony95@gmail.com>
2017-06-30 15:05:35 +0300

    new lines in lib

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -1,3 +1,12 @@
 require('rspec')
 require('pry')
 require('count_word')
+
+describe('String#count_word') do
+  it('returns the number of times a one word input appears in a sentence') do
+    expect("i".count_word("i am so i am")).to(eq(2))
+  end
+  it('returns the number of times a one word input appears in a sentence and ignores common punctuation marks') do
+    expect("we".count_word("We are Man-U, we are the red devils")).to(eq(3))
+  end
+end
